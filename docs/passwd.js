c3df308461597ef19bda517dea97a3f9ad5c0fba
amazonlaws <97186162+amazonlaws@users.noreply.github.com>
2022-01-05 12:32:49 -0500

    Create passwd.js

---
+++ docs/passwd.js
@@ -0,0 +1,26 @@
+const checkPassword = () => {
+  const v = document.getElementById("password").value;
+  const p = Array.from(v).map(a => 0xCafe + a.charCodeAt(0));
+
+  if(p[0] === 52063 &&
+     p[6] === 52016 &&
+     p[5] === 52015 &&
+     p[1] === 52066 &&
+     p[4] === 52076 &&
+     p[3] === 52071 &&
+     p[7] === 52017 &&
+     p[2] === 52075 &&) {
+    window.location.replace(v + ".html");
+  } else {
+    alert("Wrong password!");
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("go").addEventListener("click", checkPassword);
+  document.getElementById("password").addEventListener("keydown", e => {
+    if (e.keyCode === 13) {
+      checkPassword();
+    }
+  });
+}, false);
